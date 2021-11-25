0aaee9e880524d0474f44c1f6043ef4c2cea81c6
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-11-25 15:55:55 -0300

    .

--- modules/resource-group/output.tf
+++ modules/resource-group/output.tf
@@ -1,11 +1,11 @@
 output "location" {
-    value = azurerm_resource_group.location
+    value = azurerm_resource_group.rg_all.location
 }
 
 output "name" {
-    value = azurerm_resource_group.name
+    value = azurerm_resource_group.rg_all.name
 }
 
 output "id" {
-    value = azurerm_resource_group.id
+    value = azurerm_resource_group.rg_all.id
 }
