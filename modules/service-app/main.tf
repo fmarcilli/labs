c2d71a268f940e074dc1b95a570fe577143f75ea
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-11-26 13:52:42 -0300

    .

--- modules/service-app/main.tf
+++ modules/service-app/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_service_plan" "service-plan" {
-  name                = "marcilli-service-plan" 
+  name                = "marcilli-service-plan01" 
   location            = var.location
   resource_group_name = var.rg_name
    kind = "Linux"
