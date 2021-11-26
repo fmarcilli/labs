91dfddcb04298ba5ead823c039a0a5b13aa0856d
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-11-26 17:11:59 -0300

    .

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,6 +1,6 @@
 terraform {
     
-       backend "azurerm" {
+    backend "azurerm" {
     resource_group_name  = "RG-Terraform"
     storage_account_name = "terraformmarcilli"
     container_name       = "envi-lab"
