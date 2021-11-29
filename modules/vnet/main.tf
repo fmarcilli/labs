76a159932c129338a56a364e40ebfda97a943bb3
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-11-29 15:30:04 -0300

    vnet

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,22 +1,15 @@
 # VNET
 
-#resource "azurerm_virtual_machine" "vm" {
-#  name     = "VM-${var.project_name}-${var.environment}"
-#  location = var.location
-#  resource_group_name = azurerm_resource_group.rg_all.name
-
-
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg_all.name
+  resource_group_name = var.rg_name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "int"
-  resource_group_name = azurerm_resource_group.rg_all.name
+  resource_group_name = var.rg_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
@@ -24,7 +17,7 @@ resource "azurerm_subnet" "internal" {
 resource "azurerm_network_interface" "net-int" {
   name                = "example-nic"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg_all.name
+  resource_group_name = var.rg_name
 
   ip_configuration {
     name                          = "internal"
