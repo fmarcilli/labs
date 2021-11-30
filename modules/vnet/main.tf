98afc81ebfb5f4dace6769eaf305b505dac6632a
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-11-30 10:57:50 -0300

    .

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -11,18 +11,18 @@ resource "azurerm_subnet" "subnet" {
   name                 = "int"
   resource_group_name = var.rg_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = var.address_prefix
+  address_prefixes     = var.address_prefixes
   
 }
 
-resource "azurerm_network_interface" "net-int" {
-  name                = "marcilli-nic"
-  location            = var.location
-  resource_group_name = var.rg_name
+# resource "azurerm_network_interface" "net-int" {
+#   name                = "marcilli-nic"
+#   location            = var.location
+#   resource_group_name = var.rg_name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.net-int.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.net-int.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
