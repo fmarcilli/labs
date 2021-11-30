827cfb4e3ce6fa1f4954d451b30a160013a76daa
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-11-30 16:58:49 -0300

    no comment

--- modules/VM/main.tf
+++ modules/VM/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_virtual_machine" "vmlab" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "myosdisk2"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
