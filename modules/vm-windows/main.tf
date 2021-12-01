b1832fd9f1c8ea939dec4eebfef55d212cad572f
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-12-01 09:47:26 -0300

    vm windows

---
+++ modules/vm-windows/main.tf
@@ -0,0 +1,36 @@
+resource "azurerm_windows_virtual_machine" "vmwindows" {
+  name                = "VM-${var.project_name}-${var.environment}"
+  resource_group_name = var.rg_name
+  location            = var.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.net-int-vmwin.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+
+resource "azurerm_network_interface" "net-int-vmwin" {
+  name                = "NIC1-${var.project_name}-${var.environment}"
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
---
+++ modules/vm-windows/variables.tf
@@ -0,0 +1,23 @@
+variable project_name {
+  type = string
+  description = "Project Name"
+}
+
+variable environment {
+  type = string
+  description = "Environment"
+}
+
+variable location {
+  type = string
+  description = "Resource Location"
+}
+
+variable rg_name {
+  type = string
+  description = "Resource Location"
+}
+
+variable "subnet_id" {
+  type = string
+}
