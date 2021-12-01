33225e223a016a5bccd01084a11e376a455551d9
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-12-01 11:19:50 -0300

    add count storage account

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -1,5 +1,6 @@
 resource "azurerm_storage_account" "sto-account" {
-  name                     = "sto${lower(var.project_name)}${lower(var.environment)}"
+  count = 2
+  name                     = "sto${lower(var.project_name)}${lower(var.environment)}-${count.index}"
   resource_group_name      = var.rg_name
   location                 = var.location
   account_tier             = "Standard"
