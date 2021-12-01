30d3a3a1df1d402fc956441f29e412ed483c8024
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-12-01 11:30:04 -0300

    .

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "sto-account" {
   count = 2
-  name                     = "sto${lower(var.project_name)}${lower(var.environment)}-${count.index}"
+  name                     = "sto${lower(var.project_name)}${lower(var.environment)}${count.index}"
   resource_group_name      = var.rg_name
   location                 = var.location
   account_tier             = "Standard"
