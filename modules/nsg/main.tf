e565499aba9d2c875da031a6440e88e2d8cc1f04
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-12-06 09:51:28 -0300

    nsg semente modulo

---
+++ modules/nsg/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_network_security_group" "nsg-marcilli" {
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = var.rg_name
+}
+
+resource "azurerm_network_security_rule" "nsg-marcilli" {
+  name                        = "test123"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.rg_name
+  network_security_group_name = var.nsg_name
+}
