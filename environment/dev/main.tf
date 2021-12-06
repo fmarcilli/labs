71a1324056f160d970c2ce4df5eda10b60466db9
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-12-06 14:13:55 -0300

    main mod nsg subnet_id

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -77,7 +77,7 @@ module "nsg-lab" {
   vnet_name           = "vnetlab"
   address_space       = ["10.0.0.0/16"]
   address_prefixes    = ["10.0.2.0/24"]
-  
+  subnet_id           = module.vnet_lab.subnet_id
     
  }
 
