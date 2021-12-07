5200a90a7bfe58b74bf1ed482095fec772ae2d67
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-12-07 17:37:11 -0300

    comment_all

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -12,14 +12,14 @@ provider "azurerm" {
     features {}
  }
 
-# Resource Group Module
-module "resource_group_lab"{
-  source = "../../modules/resource-group"
-  project_name        = "FilipeLABS"
-  environment         = "dev"
-  location            = "EastUS"
+# # Resource Group Module
+# module "resource_group_lab"{
+#   source = "../../modules/resource-group"
+#   project_name        = "FilipeLABS"
+#   environment         = "dev"
+#   location            = "EastUS"
    
-}
+# }
 
 # # Storage Account Module
 # module "storage_account_lab"{
@@ -32,18 +32,18 @@ module "resource_group_lab"{
     
 # }
 
-#VNET Module
-module "vnet_lab"{
-  source = "../../modules/vnet"
-  project_name        = "FilipeLABS"
-  environment         = "dev"
-  location            = module.resource_group_lab.location
-  rg_name             = module.resource_group_lab.name
-  vnet_name           = "vnetlab"
-  address_space       = ["10.0.0.0/16"]
-  address_prefixes    = ["10.0.2.0/24"]
+# #VNET Module
+# module "vnet_lab"{
+#   source = "../../modules/vnet"
+#   project_name        = "FilipeLABS"
+#   environment         = "dev"
+#   location            = module.resource_group_lab.location
+#   rg_name             = module.resource_group_lab.name
+#   vnet_name           = "vnetlab"
+#   address_space       = ["10.0.0.0/16"]
+#   address_prefixes    = ["10.0.2.0/24"]
   
-  }
+#   }
   
 # # VM Module Windows
 # module "vm_windows"{
@@ -67,20 +67,20 @@ module "vnet_lab"{
    
 # # }
 
-#NSG Module 
-module "nsg-lab" {
-  source = "../../modules/nsg"
-  project_name        = "FilipeLABS"
-  environment         = "dev"
-  location            = module.resource_group_lab.location
-  rg_name             = module.resource_group_lab.name
-  vnet_name           = "vnetlab"
-  address_space       = ["10.0.0.0/16"]
-  address_prefixes    = ["10.0.2.0/24"]
-  subnet_id           = module.vnet_lab.subnet_id
+# NSG Module 
+# module "nsg-lab" {
+#   source = "../../modules/nsg"
+#   project_name        = "FilipeLABS"
+#   environment         = "dev"
+#   location            = module.resource_group_lab.location
+#   rg_name             = module.resource_group_lab.name
+#   vnet_name           = "vnetlab"
+#   address_space       = ["10.0.0.0/16"]
+#   address_prefixes    = ["10.0.2.0/24"]
+#   subnet_id           = module.vnet_lab.subnet_id
  
     
- }
+#  }
 
 
 
