783b2f6533cbfaa5e3cae07354dc11d11a2b3d25
Filipe Marcilli <filipe.marcilli@hotmail.com>
2021-12-10 15:42:50 -0300

    NSG rules

--- modules/nsg/main.tf
+++ modules/nsg/main.tf
@@ -4,36 +4,52 @@ resource "azurerm_network_security_group" "nsg-lab" {
   resource_group_name = var.rg_name
 }
 
-# resource "azurerm_network_security_rule" "nsg-sec_rule-lab" {
-#   name                        = "RDP-Rule"
-#   priority                    = 100
-#   direction                   = "Inbound"
-#   access                      = "Allow"
-#   protocol                    = "Tcp"
-#   source_port_range           = "*"
-#   destination_port_range      = "3389"
-#   source_address_prefix       = "*"
-#   destination_address_prefix  = "*"
-#   resource_group_name         = var.rg_name
-#   network_security_group_name = azurerm_network_security_group.nsg-lab.name
-
-# }
-
-security_rule{
-  name = "rule-1"
-   priority                   = 100
-   direction                  = "Inbound"
-   access                     = "Deny"
-   protocol                   = "*"
-   source_port_range          = "*"
-   destination_port_range     = "3389"
-   source_address_prefix       = "*"
-   destination_address_prefix  = "*"
-   resource_group_name         = var.rg_name
-   network_security_group_name = azurerm_network_security_group.nsg-lab.name
-   
+resource "azurerm_network_security_rule" "nsg-sec_rule-lab" {
+  name                        = "RDP-Rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.rg_name
+  network_security_group_name = azurerm_network_security_group.nsg-lab.name
 
 }
+
+resource "azurerm_network_security_rule" "nsg-sec_rule-lab-02" {
+  name                        = "RDP-Rule"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.rg_name
+  network_security_group_name = azurerm_network_security_group.nsg-lab.name
+
+}
+
+
+# security_rule{
+#   name = "rule-1"
+#    priority                   = 100
+#    direction                  = "Inbound"
+#    access                     = "Deny"
+#    protocol                   = "*"
+#    source_port_range          = "*"
+#    destination_port_range     = "3389"
+#    source_address_prefix       = "*"
+#    destination_address_prefix  = "*"
+#    resource_group_name         = var.rg_name
+#    network_security_group_name = azurerm_network_security_group.nsg-lab.name
+   
+
+# }
 
 
 resource "azurerm_subnet_network_security_group_association" "association-nsg" {
